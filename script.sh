43ddfbf9ddf2755cbdc97ea563500cfccf9f0862
harbinman <harbinman@163.com>
2024-07-22 18:54:07 +0800

    add more logic

--- script.sh
+++ script.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 apt-get update && apt upgrade -y
+
+#install docker
 curl -sSL https://get.docker.com | bash
+
+#install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+mv kubectl /usr/local/bin
+
+#install kind
+
+# For AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.23.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
