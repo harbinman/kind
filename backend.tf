4cec0ca158be36000820fdea669279c5b1c3630c
harbinman <harbinman@163.com>
2024-07-22 17:38:01 +0800

    modify backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraformstatefilebucket2077"
-    key    = "env:/prod/key/terraform/terraform.state"
+    key    = "env:/prod/key/terraform/terraform.tfstate"
     region = "ap-southeast-1"
     dynamodb_table = "backend"
   }
